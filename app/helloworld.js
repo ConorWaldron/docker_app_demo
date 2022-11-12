8c882ea22c113e0268e20a61d4d0b6972da4385b
conor <conor.waldron@gmail.com>
2022-11-12 16:44:21 +0000

    adding java script helloworld

---
+++ app/helloworld.js
@@ -0,0 +1,8 @@
+// JS is not supported by PyCharm community edition
+// but you can run this if you have installed node JS with the command 'node helloworld.js'
+
+function sayHello(name) {
+    console.log('Hello' + name);
+}
+
+sayHello('Conor')
